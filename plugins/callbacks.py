fe7957151e474afc4d8c4f3a809f63e6522cd696
SkullGG-Ui <84499653+SkullGG-Ui@users.noreply.github.com>
2022-01-26 11:30:40 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Maintaince By",url="https://t.me/Skull_Bots"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/Skull_Index_Bot"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/Skull_Index_Help")
                 ]
             ]
         )
